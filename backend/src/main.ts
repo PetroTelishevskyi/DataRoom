247ddcee892ef30a0b9d0946d34fa615662fd2b5
Petro Telishevskyi <ptelishewsky@gmail.com>
2026-08-18 22:12:55 +0300

    Fix cookie-parser import for Vercel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,32 +1,32 @@
-import { ValidationPipe } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { NestFactory } from "@nestjs/core";
-import * as cookieParser from "cookie-parser";
-import { AppModule } from "./app.module";
-import { ApiExceptionFilter } from "./common/filters/api-exception.filter";
+import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { NestFactory } from '@nestjs/core'
+import cookieParser from 'cookie-parser'
+import { AppModule } from './app.module'
+import { ApiExceptionFilter } from './common/filters/api-exception.filter'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  const frontendUrl = configService.getOrThrow<string>("FRONTEND_URL");
+	const app = await NestFactory.create(AppModule)
+	const configService = app.get(ConfigService)
+	const frontendUrl = configService.getOrThrow<string>('FRONTEND_URL')
 
-  app.setGlobalPrefix("api/v1");
-  app.enableCors({
-    origin: frontendUrl,
-    credentials: true,
-  });
-  app.use(cookieParser());
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-    }),
-  );
-  app.useGlobalFilters(new ApiExceptionFilter());
-  app.enableShutdownHooks();
+	app.setGlobalPrefix('api/v1')
+	app.enableCors({
+		origin: frontendUrl,
+		credentials: true,
+	})
+	app.use(cookieParser())
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			transform: true,
+		}),
+	)
+	app.useGlobalFilters(new ApiExceptionFilter())
+	app.enableShutdownHooks()
 
-  const port = configService.get<number>("PORT", 3000);
-  await app.listen(port);
+	const port = configService.get<number>('PORT', 3000)
+	await app.listen(port)
 }
 
-void bootstrap();
+void bootstrap()
